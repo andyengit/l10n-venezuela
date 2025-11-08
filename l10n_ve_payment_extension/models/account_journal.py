49e389e267820a2ec48a09fd84504893a3946a87
Andyclodofy <anderson.armeya@gmail.com>
2025-11-08 08:04:34 -0400

    [FIX] l10n_ve_payment_extension

--- l10n_ve_payment_extension/models/account_journal.py
+++ l10n_ve_payment_extension/models/account_journal.py
@@ -8,13 +8,13 @@ _logger = logging.getLogger(__name__)
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    default_account_id = fields.Many2one(
-        domain=(
-            "[('deprecated', '=', False), ('company_id', '=', company_id),"
-            "'|',('account_type', '=', default_account_type),"
-            "('account_type', 'in', ('income', 'income_other') if type == 'sale' else ('expense', 'expense_depreciation', 'expense_direct_cost') if type == 'purchase' else ('asset_current', 'liability_current'))]"
-        )
-    )
+    # default_account_id = fields.Many2one(
+    #     domain=(
+    #         "[('deprecated', '=', False), ('company_id', '=', company_id),"
+    #         "'|',('account_type', '=', default_account_type),"
+    #         "('account_type', 'in', ('income', 'income_other') if type == 'sale' else ('expense', 'expense_depreciation', 'expense_direct_cost') if type == 'purchase' else ('asset_current', 'liability_current'))]"
+    #     )
+    # )
 
     @api.depends("type", "currency_id")
     def _compute_inbound_payment_method_line_ids(self):
@@ -39,7 +39,9 @@ class AccountJournal(models.Model):
                 journal_ids |= journal
                 journal.inbound_payment_method_line_ids = pay_method_line_ids_commands
 
-        return super(AccountJournal, self - journal_ids)._compute_inbound_payment_method_line_ids()
+        return super(
+            AccountJournal, self - journal_ids
+        )._compute_inbound_payment_method_line_ids()
 
     @api.depends("type", "currency_id")
     def _compute_outbound_payment_method_line_ids(self):
@@ -64,4 +66,6 @@ class AccountJournal(models.Model):
                 journal_ids |= journal
                 journal.outbound_payment_method_line_ids = pay_method_line_ids_commands
 
-        return super(AccountJournal, self - journal_ids)._compute_outbound_payment_method_line_ids()
+        return super(
+            AccountJournal, self - journal_ids
+        )._compute_outbound_payment_method_line_ids()
