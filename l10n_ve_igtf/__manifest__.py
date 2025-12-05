53ef6afe56b5c7decaf15c3f6c91745c1c69b958
Andyclodofy <anderson.armeya@gmail.com>
2025-12-05 01:43:17 +0000

    [MIG] l10n_ve_igtf: Migration to 18.0

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "binaural-dev",
     "website": "https://github.com/OCA/l10n-venezuela",
     "category": "Accounting/Accounting",
-    "version": "17.0.0.0.17",
+    "version": "18.0.1.0.0",
     "depends": [
         "base",
         "account",
