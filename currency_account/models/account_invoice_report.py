e920e71823e9a4182d3f98fa71f3b8e48f1e11bb
andyengit <anderson.armeya@gmail.com>
2026-01-17 16:14:56 +0000

    [REMOVE] Comment out amount_currency_usd field and related SQL selection in account invoice report and views for future reference.

--- currency_account/models/account_invoice_report.py
+++ currency_account/models/account_invoice_report.py
@@ -5,11 +5,11 @@ class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
 
-    amount_currency_usd = fields.Float(
-        string="Saldo en USD",
-        help="Saldo en USD",
-    )
+    # amount_currency_usd = fields.Float(
+    #     string="Saldo en USD",
+    #     help="Saldo en USD",
+    # )
 
-    @api.model
-    def _select(self):
-        return SQL("%s,line.amount_currency_usd as amount_currency_usd ", super()._select())
+    # @api.model
+    # def _select(self):
+    #     return SQL("%s,line.amount_currency_usd as amount_currency_usd ", super()._select())
