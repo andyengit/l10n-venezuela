01bd54bcf8cbc6af9ce137b9d221a7394b86fb4f
andyengit <anderson.armeya@gmail.com>
2026-02-07 17:32:38 +0000

    [ADD] Introduce credit sale authorization module: implement fields and logic for credit sale management in account move and sale order, including security group for authorization and corresponding views for user interaction.

---
+++ credit_sale_authorization/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Autorización de Ventas a Crédito",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "summary": "Control de autorización para confirmar ventas y facturas a crédito",
+    "author": "andyengit",
+    "maintainer": "andyengit",
+    "website": "https://github.com/OCA/l10n-venezuela",
+    "depends": ["sale", "account"],
+    "data": [
+        "security/security.xml",
+        "views/sale_order_views.xml",
+        "views/account_move_views.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
+}
